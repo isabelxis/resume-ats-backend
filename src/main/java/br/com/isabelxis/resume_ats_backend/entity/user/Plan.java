3a90ea459dabf3f2dd396d5f777eec4be7e88154
ixsilva <ixsilva@sfiec.org.br>
2026-02-10 17:25:49 -0300

    feat: entity

---
+++ src/main/java/br/com/isabelxis/resume_ats_backend/entity/user/Plan.java
@@ -0,0 +1,8 @@
+package br.com.isabelxis.resume_ats_backend.entity.user;
+
+public enum Plan {
+    FREE,
+    PRO,
+    PREMIUM
+    
+}
---
+++ src/main/java/br/com/isabelxis/resume_ats_backend/entity/user/User.java
@@ -0,0 +1,72 @@
+package br.com.isabelxis.resume_ats_backend.entity.user;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Plan plan = Plan.FREE;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {    
+        return true;
+    }   
+    
+}
